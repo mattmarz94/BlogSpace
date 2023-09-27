880cce47661873718832a52c1c3b835bd37bf009
Matt Marzano <122555133+mattmarz94@users.noreply.github.com>
2023-09-26 23:45:08 -0400

    created an eventlistener for the new post which will submit the new blog. Also, created a function which will combine the title/body value into an object and used the fetch method to send the blog to the server.

--- app.js
+++ app.js
@@ -12,3 +12,27 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
         }
         document.getElementById("blog-list").innerHTML = html
     })
+
+document.getElementById("new-post").addEventListener("submit", function(e) {
+    e.preventDefault()
+    const postTitle = document.getElementById("post-title").value
+    const postBody = document.getElementById("post-body").value
+    const data = {
+        title: postTitle,
+        body: postBody
+    }
+    
+    const options = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+})
